7a19c6f4c06c7af08f8687c83932d59bf32d3ef1
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2023-06-21 23:23:45 -0300

    PovoamentoTabelaEndereco
    Script contendo os INSERTs na Tabela "Endereco"

---
+++ PovoamentoTabelaEndereco.sql
@@ -0,0 +1,17 @@
+INSERT INTO `Endereco` (`CPF_Usuario`,`Cidade`,`CEP`,`UF`,`Pais`)
+VALUES
+  ('00163103280',"Osasco","55361715","SP","Brazil"),
+  ('04741556726',"Itabuna","78792300","BA","Brazil"),
+  ('08104676386',"Rio Verde","19564376","GO","Brazil"),
+  ('08787625871',"Rio de Janeiro","84671577","RJ","Brazil"),
+  ('13484973871',"Anápolis","69314591","GO","Brazil"),
+  ('15569390575',"Curitiba","02877977","PR","Brazil"),
+  ('35309116772',"Maranguape","14201489","CE","Brazil"),
+  ('40130914378',"Ilhéus","42502863","BA","Brazil"),
+  ('46383161476',"São Gonçalo","62266773","RJ","Brazil"),
+  ('61518611469',"Timon","16660424","MA","Brazil"),
+  ('61853174515',"Crato","89732971","CE","Brazil"),
+  ('75328586914',"Divinópolis","11733767","MG","Brazil"),
+  ('83644347352',"Mogi das Cruzes","60536033","SP","Brazil"),
+  ('87976676584',"Fortaleza","82678560","CE","Brazil"),
+  ('88917753137',"Sete Lagoas","06122204","MG","Brazil");
