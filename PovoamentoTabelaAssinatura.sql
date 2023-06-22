1fbf776222202b0ec2140d8b8eb10ceb7a051dad
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2023-06-21 23:22:38 -0300

    PovoamentoTabelaAssinatura
    Script contendo os INSERTs na Tabela "Assinatura"

---
+++ PovoamentoTabelaAssinatura.sql
@@ -0,0 +1,17 @@
+INSERT INTO `Assinatura` (`Id_Usuario`, `Senha`, `CPF_Usuario`, `Tipo_Plano`) 
+VALUES
+	("Lewis_Simpson","PXQ04STV5X",'00163103280',"Premium"),
+	("Alexander_Sims","SAX24XTS3F",'04741556726',"Supremo"),
+	("Nita_Duncan","EMN57YTN2J",'08104676386',"Premium"),
+	("Colin_Holt","NWF54GZS2B",'08787625871',"Premium"),
+	("Angela_Lucas","XBG21CZLNH",'13484973871',"Supremo"),
+	("Elijah_Bennett","MKI43DL8OK",'15569390575',"Padrão"),
+	("Nolan_Banks","WQF23HQ7LO",'35309116772',"Padrão"),
+	("Gavin_Holland","CHC6JEQ9MQ",'40130914378',"Padrão"),
+	("Patrick_Middleton","AS27MBJ4MX",'46383161476',"Premium"),
+	("Yvonne_Bauer","LRS8ZHS2BY",'61518611469',"Supremo"),
+    ("Madeline_Cardenas","JO24QXJ7GS",'61853174515',"Premium"),
+	("Vernon_Daugherty","ZR05SYV3VZ",'75328586914',"Básico"),
+	("Uriah_Mcguire","XSG3BDC8TP",'83644347352',"Básico"),
+	("Declan_Mcdonald","JN26ZYU8LW",'87976676584',"Básico"),
+	("Nell_Suarez","GVG91TW2LG",'88917753137',"Supremo");
