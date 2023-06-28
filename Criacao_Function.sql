d38a20f206af8be72b099e0944062d4523b57498
gabrielvaz081 <vazgabriel081@gmail.com>
2023-06-28 18:11:56 -0300

    Criação das Functions

--- Criacao_Function.sql
+++ Criacao_Function.sql
@@ -1,32 +1,23 @@
 DELIMITER $$
-CREATE FUNCTION Alterar_Plano( Pessoa VARCHAR(20), Plano VARCHAR(10) )
-RETURNS VARCHAR(10)
+CREATE FUNCTION verificar_usuario(cpf varchar(11)) 
+RETURNS boolean 
+deterministic
 BEGIN
-DECLARE Tipo_Plano VARCHAR(10);
-IF Plano != Tipo_Plano THEN
-    select count(Usuario) as Num_Perfil from Perfil where Perfil.Usuario = Pessoa;
-    if Num_Perfil < Quant_Perfil then
-	insert into Perfil(`Nome`, `Usuario`, `Controle_Pais`) values;
-    elseif Num_Perfil > Quant_Perfil then
-    delete Usuario from Perfil where Perfil.Usuario = Pessoa;
-    end if;
-END IF;
-RETURN (Quant_Perfil);
-END$$
+   DECLARE is_user BOOLEAN;
+    SELECT EXISTS (
+    SELECT 1
+    FROM usuario
+    WHERE CPF = usuario.`CPF` 
+  ) INTO is_user;
+     RETURN is_user;
+END $$
 
-CREATE FUNCTION quant_assinaturas (Plano VARCHAR(10))
-RETURNS INT
+CREATE FUNCTION contar_assinatura(plano varchar(10)) 
+RETURNS int 
+deterministic
 BEGIN
-DECLARE quant_assinatura int;
-IF Plano = 'Básico' THEN
-set (select * from `Quant_Assinatura` where `Tipo_Plano` = 'Básico');
-ELSEIF Plano = 'Padrão' THEN
-set (select * from `Quant_Assinatura` where `Tipo_Plano` = 'Padrão');
-ELSEIF Plano = 'Premium' THEN
-set (select * from `Quant_Assinatura` where `Tipo_Plano` = 'Premium');
-ELSEIF Plano = 'Supremo' THEN
-set (select * from `Quant_Assinatura` where `Tipo_Plano` = 'Supremo');
-END IF;
-RETURN (quant_assinaturas);
-END$$
+    DECLARE quant int;
+    select `Quant_Assinatura` from `Quant_Assinatura` where `Tipo_Plano` = plano into quant;
+    RETURN quant;
+END $$
 DELIMITER ;
